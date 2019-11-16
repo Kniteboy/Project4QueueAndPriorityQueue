fe62d3e19125b5f027a46667f855edac1594251d
eyong0 <57815391+eyong0@users.noreply.github.com>
2019-11-16 17:23:00 -0500

    ConventionRegistration.cs Progress
    progress

--- Project4SimulationWithQueuesAndPriorityQueues/ConventionRegistration.cs
+++ Project4SimulationWithQueuesAndPriorityQueues/ConventionRegistration.cs
@@ -21,52 +21,139 @@ namespace Project4SimulationWithQueuesAndPriorityQueues
     /// </summary>
     class ConventionRegistration
     {
+        private Random r;
         private int maxLineCount;
         private PriorityQueue<Event> PQ;
         private List<Queue<Registrant>> regLines;
+        private int actualNumberOfRegistrants;
+        private DateTime openTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 8, 0, 0);
+        private DateTime closingTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 18, 0, 0);
 
-        private DateTime startTime;
 
-        /// <summary>
-        /// Property that gets or sets the start time.
-        /// </summary>
-        /// <value>
-        /// The convention's start time.
-        /// </value>
-        public DateTime StartTime
-        {
-            get { return startTime; }
-            set { startTime = value; }
-        } //end StartTime
-
-        private DateTime closingTime;
+        private int expectedNumberOfRegistrants;
 
         /// <summary>
-        /// Property that gets or sets the closing time.
+        /// Property that gets or sets the expected number of registrants.
         /// </summary>
         /// <value>
-        /// The convention's closing time.
+        /// The expected number of registrants at the convention.
         /// </value>
-        public DateTime ClosingTime
+        public int ExpectedNumberOfRegistrants
         {
-            get { return closingTime; }
-            set { closingTime = value; }
-        } //end ClosingTime
+            get { return expectedNumberOfRegistrants; }
+            set { expectedNumberOfRegistrants = value; }
+        } //end ExpectedNumberOfRegistrants
+
+        private int hoursOfOperation;
+
+        /// <summary>
+        /// Property that gets or sets the hours of operation.
+        /// </summary>
+        /// <value>
+        /// The convention's hours of operation.
+        /// </value>
+        public int HoursOfOperation
+        {
+            get { return hoursOfOperation; }
+            set { hoursOfOperation = value; }
+        } //end HoursOfOperation
+
+        private int numberOfWindows;
+
+        /// <summary>
+        /// Property that gets or sets the number of registration windows.
+        /// </summary>
+        /// <value>
+        /// The number of registration windows at the convention.
+        /// </value>
+        public int NumberOfWindows
+        {
+            get { return numberOfWindows; }
+            set { numberOfWindows = value; }
+        } //end NumberOfWindows
+
+        private double checkoutDuration;
+
+        /// <summary>
+        /// Property that gets or sets the expected checkout duration.
+        /// </summary>
+        /// <value>
+        /// The expected checkout duration per customer.
+        /// </value>
+        public double CheckoutDuration
+        {
+            get { return checkoutDuration; }
+            set { checkoutDuration = value; }
+        } //end CheckoutDuration
 
         /// <summary>
         /// Default constructor that initializes a new instance of the <see cref="ConventionRegistration"/> class.
         /// </summary>
         public ConventionRegistration()
         {
+            r = new Random();
             maxLineCount = 0;
             PQ = new PriorityQueue<Event>();
             regLines = new List<Queue<Registrant>>();
-            startTime = new DateTime();
-            closingTime = new DateTime();
+            expectedNumberOfRegistrants = 1000;
+            hoursOfOperation = 10;
+            numberOfWindows = 1;
+            checkoutDuration = 5.5;
 
         } //end ConventionRegistration()
 
-        public ConventionRegistration()
+        /// <summary>
+        /// Runs the Convention Registration simulation.
+        /// </summary>
+        public void RunSimulation()
+        {
+
+        } //end RunSimulation()
+
+        public void GenerateEvents()
+        {
+            TimeSpan start;
+            TimeSpan interval;
+            actualNumberOfRegistrants = Poisson(expectedNumberOfRegistrants);
+
+            for (int i = 1; i <= actualNumberOfRegistrants; i++)
+            {
+                start = new TimeSpan(0, r.Next(10 * 60), 0);
+                interval = new TimeSpan(0, (int)(1.5 + NegativeExponential(checkoutDuration)), 0);  //min time: 1 minute & 30 seconds
+
+                PQ.Enqueue(new Event(EVENTTYPE.ARRIVAL, openTime.Add(start), i));
+                PQ.Enqueue();
+            } //end for (int i = 0; i < actualNumberOfRegistrants; i++)
+
+        } //end GenerateEvents()
+
+        #region Random Distributions
+        /// <summary>
+        /// The method generating numbers from a Poisson distribution for the registrant's display number
+        /// </summary>
+        /// <param name="ExpectedValue">The expected number of registrants</param>
+        /// <returns>a number from the Poisson distribution</returns>
+        private int Poisson(double ExpectedValue)
+        {
+            double dLimit = -ExpectedValue;
+            double dSum = Math.Log(Registrant.r.NextDouble());
+
+            int Count;
+            for (Count = 0; dSum > dLimit; Count++)
+                dSum += Math.Log(Registrant.r.NextDouble());
+            return Count;
+        } //end Poisson(double ExpectedValue)
+
+        /// <summary>
+        /// The negative exponential function determines how long an individual stays at the registration booth (after getting out of the line)
+        /// </summary>
+        /// <param name="ExpectedValue">The expected time taken by each registrant</param>
+        /// <returns>The time taken at the registration booth</returns>
+        private double NegativeExponential(double ExpectedValue)
+        {
+            return -ExpectedValue * Math.Log(r.NextDouble(), Math.E);
+        } //end NegativeExponential(double ExpectedValue)
+        #endregion distribution code
 
     } //end ConventionRegistration
 }
